bc126f58ce437dafaa48737b1bacd47fb2b0eb30
shoaibrrian <shoaibrian@gmail.com>
2025-07-14 16:22:09 +0600

    update balance

--- js/home.js
+++ js/home.js
@@ -4,6 +4,8 @@
  * Step1: add event handler
  * prevent page reload after form submit
  * Step2: get money to be added to the account balance
+ * Step3: verify pin
+ * Step4: Get the current balance
  */
 
 // step1: add an event handler to the add money button inside the form
@@ -17,7 +19,27 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     console.log(addMoneyInput, );
 
     // get the pin number provided 
-    const addPinInput = document.getElementById('input-pin').value;
-    
-    console.log(addPinInput)
+    const pinNumber = document.getElementById('input-pin').value;
+
+    console.log(pinNumber)
+
+    // Step3: verify pin
+    if(pinNumber === '1234'){
+        // Step4: Get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
+
+        // step5: add  addMOneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const updateBalance = addMoneyNumber + balanceNumber
+        console.log(updateBalance);
+
+
+    // step6: update the balance in the UI
+    document.getElementById('account-balance').innerText = updateBalance;
+    }
+    else{
+        alert('Failed to add money. Please Try Again!')
+    }
 })
